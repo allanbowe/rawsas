3436b886400045e24965a6480cd7e81cfe576402
blog-dev <blog-dev@users.noreply.github.com>
2026-09-02 00:49:42 +0100

    feat: dedicated tall ad for Macro Dash sponsor
    Remove the article link, replace the screenshot with a
    purpose-made 240x400 ad featuring the running man icon,
    gradient title, subtitle, feature bullets and play button.
    Clean up the old screenshot image.

--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import DC from '../../images/data-controller.png'
 import SasApp from '../../images/sasapp.png'
 import BuildApps from '../../images/buildapps240x400.png'
-import MacroDash from '../../images/macro-dash.png'
+import MacroDash from '../../images/macro-dash-ad.png'
 
 import './styles.scss'
 
@@ -13,7 +13,6 @@ const Sponsors: React.FC = () => {
       title: 'Macro Dash - the SAS Powered Platformer',
       href: 'https://dash.sasjs.io',
       img: MacroDash,
-      article: 'https://sasapps.io/macro-dash',
     },
     {
       title: 'Data Controller',
@@ -39,16 +38,6 @@ const Sponsors: React.FC = () => {
           <a href={sp.href} target="_blank" rel="noopener noreferrer">
             <img src={sp.img} alt={sp.title} title={sp.title} />
           </a>
-          {sp.article && (
-            <a
-              className="sponsor-article"
-              href={sp.article}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read the SAS Apps article →
-            </a>
-          )}
         </div>
       ))}
     </div>

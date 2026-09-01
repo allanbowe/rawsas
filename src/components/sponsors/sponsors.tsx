8ca8dc00fa6dd5f59198f7f71e2fbfe93ece1734
blog-dev <blog-dev@users.noreply.github.com>
2026-09-02 00:26:31 +0100

    feat: add Macro Dash sponsor to right sidebar
    Links the game image to https://dash.sasjs.io and adds a
    'Read the SAS Apps article' link to https://sasapps.io/macro-dash.

--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -3,11 +3,18 @@ import React from 'react'
 import DC from '../../images/data-controller.png'
 import SasApp from '../../images/sasapp.png'
 import BuildApps from '../../images/buildapps240x400.png'
+import MacroDash from '../../images/macro-dash.png'
 
 import './styles.scss'
 
 const Sponsors: React.FC = () => {
   const sponsors = [
+    {
+      title: 'Macro Dash - the SAS Powered Platformer',
+      href: 'https://dash.sasjs.io',
+      img: MacroDash,
+      article: 'https://sasapps.io/macro-dash',
+    },
     {
       title: 'Data Controller',
       href: 'https://datacontroller.io/',
@@ -28,9 +35,21 @@ const Sponsors: React.FC = () => {
     <div className="sponsors">
       <h4>Sponsors</h4>
       {sponsors.map((sp, i) => (
-        <a key={i} href={sp.href} target="_blank" rel="noopener noreferrer">
-          <img src={sp.img} alt={sp.title} title={sp.title} />
-        </a>
+        <div key={i} className="sponsor-item">
+          <a href={sp.href} target="_blank" rel="noopener noreferrer">
+            <img src={sp.img} alt={sp.title} title={sp.title} />
+          </a>
+          {sp.article && (
+            <a
+              className="sponsor-article"
+              href={sp.article}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read the SAS Apps article →
+            </a>
+          )}
+        </div>
       ))}
     </div>
   )
